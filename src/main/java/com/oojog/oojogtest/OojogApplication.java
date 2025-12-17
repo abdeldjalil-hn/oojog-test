278f51b0a3afb34d714556b46dc0f8c9e5dc4392
abdeldjalil <abdeldjalil@cqr.com.sa>
2025-12-17 21:41:57 +0000

    initial commit

---
+++ src/main/java/com/oojog/oojogtest/OojogApplication.java
@@ -0,0 +1,13 @@
+package com.oojog.oojogtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OojogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OojogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oojog/oojogtest/OojogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oojog.oojogtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OojogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
